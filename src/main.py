94860e47a2050aa76c1718607b015ae5893cab77
jklypchak13 <jarod.klypchak@gmail.com>
2019-09-14 22:32:58 -0400

    Pulled up submodule

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ sys.path.append('lib/')
 
 import bot_helper.bot.client as client
 import bot_helper.resources.message_handler as message_handler
+import bot_helper.resources.spreadsheet as spreadsheet
 
 
 def sample_function(message):
@@ -33,6 +34,9 @@ client = client.Client()
 # Register Callback Functions
 client.register_on_message_callback(sample_function, [])
 client.register_on_message_callback(processes_wrapper, [handler])
+
+# Make Spreadsheet
+sheet = spreadsheet.Spreadsheet(keys['sheet_id'], 'data/token.json', 'data/credentials.json')
 
 # Start Client
 client.run(keys['discord_token'])
